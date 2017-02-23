957a2c3093b982aa6df7947814d5456ddd4ea104
jiangchengshan <735231195@qq.com>
2017-02-23 12:28:05 +0800

    update

--- task16/task16.js
+++ task16/task16.js
@@ -7,10 +7,10 @@
  * };
  */
 var aqiData = {};
-aqiData = {
-    "北京": 90,
-    "上海": 40
-};
+// aqiData = {
+//     "北京": 90,
+//     "上海": 40
+// };
 /**
  * 从用户输入中获取数据，向aqiData中增加一条数据
  * 然后渲染aqi-list列表，增加新增的数据
@@ -46,19 +46,18 @@ function addAqiData() {
  */
 function renderAqiList() {
     var aqiTable = document.getElementById('aqi-table');
-    var tHead = document.createElement('thead');
-    var hTr = document.createElement('tr');
-    var array = ['城市','空气质量','操作'];
-    for(let i in array){
-        var oTh = document.createElement('th');
-        oTh.innerHTML = array[i];
-        hTr.appendChild(oTh);
-        tHead.appendChild(hTr);
-    }
-    aqiTable.appendChild(tHead);
+    // var tHead = document.createElement('thead');
+    // var hTr = document.createElement('tr');
+    // var array = ['城市','空气质量','操作'];
+    // for(let i in array){
+    //     var oTh = document.createElement('th');
+    //     oTh.innerHTML = array[i];
+    //     hTr.appendChild(oTh);
+    //     tHead.appendChild(hTr);
+    // }
+    // aqiTable.appendChild(tHead);
 
     for(let i in aqiData){
-        alert(i);
         var oTr = document.createElement('tr');
         var oTd = document.createElement('td');
 
@@ -103,7 +102,8 @@ function delBtnHandle() {
 }
 
 function init() {
-
+    var addBtn = document.getElementById('add-btn');
+    addBtn.onclick = addBtnHandle;
     // 在这下面给add-btn绑定一个点击事件，点击时触发addBtnHandle函数
 
     // 想办法给aqi-table中的所有删除按钮绑定事件，触发delBtnHandle函数
