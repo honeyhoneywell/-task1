47f285f881c67fc51b3bd3a8ec781e071704ab32
jiangchengshan <735231195@qq.com>
2017-03-09 18:48:45 +0800

    任务三十四：听指令的小方块（二）

--- task34/task34.js
+++ task34/task34.js
@@ -18,7 +18,7 @@ var data = {
 //运转
 function control() {
     var box = document.getElementById('box');
-    var command = document.getElementsByTagName('input')[0].value;
+    var command = document.getElementsByTagName('input')[0].value.trim();
     switch (command){
         case "TUN LEF":
             data.dir--;
@@ -28,6 +28,7 @@ function control() {
         case "TUN RIG":
             data.dir++;
             if(data.dir>3){data.dir = data.dir-4}
+            box.style.transform = "rotate(90deg)";
             turn(10,9);
             break;
         case "TUN BAC":
@@ -36,6 +37,30 @@ function control() {
             break;
         case "GO":
             doGo(data.dir);
+            break;
+        case "TRA LEF":
+            doGo(2);
+            break;
+        case "TRA TOP":
+            doGo(3);
+            break;
+        case "TRA RIG":
+            doGo(0);
+            break;
+        case "TRA BOT":
+            doGo(1);
+            break;
+        case "MOV LEF":
+            data.dir=2;
+            break;
+        case "MOV RIG":
+            data.dir=0;
+            break;
+        case "MOV TOP":
+            data.dir=3;
+            break;
+        case "MOV BOT":
+            data.dir=1;
             break;
     }
 }
@@ -55,37 +80,39 @@ function doGo(dir){
         case (dir===3 && disY<=30):
             alert('到头了');
             break;
-        default:
-            goTo(box,data.dir);
+        case (dir===0 || dir===2):
+            goTo(box,'left',dir);
+            break;
+        case (dir===1 || dir===3):
+            goTo(box,'top',dir);
+            break;
     }
 }
-
 //前进动画
 function goTo(obj,attr,dir){
     if(dir===2||dir===3){dir=-1}else{dir=1}
     var target = 0;
     var dis = parseFloat(getStyle(obj,attr));
     var timer = setInterval(function () {
-        target += 5;
+        target += 2;
         if(target===30){
             clearInterval(timer);
         }
         obj.style[attr] = (dis + target*dir) + 'px';
     },20)
 }
-
 //旋转动画
-function turn(speed,target,endFn) {
+function turn(speed,target,endFn){
     var box = document.getElementById('box');
     var dir = 0;
     var timer = setInterval(function () {
         dir += speed;
         data.deg += speed;
+        box.style.transform = "rotate("+ data.deg +"deg)";
         if(Math.abs(dir/speed)===target){
             clearInterval(timer);
             endFn&&endFn();
         }
-        box.style.transform = "rotate("+ data.deg +"deg)";
     },20)
 }
 
