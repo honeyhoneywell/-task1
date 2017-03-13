35f847dfd646b11b80541866d4a7b2fe61075f25
jiangchengshan <735231195@qq.com>
2017-03-13 18:21:51 +0800

    任务三十七：UI组件之浮出层

--- task37/task37.js
+++ task37/task37.js
@@ -1,6 +1,16 @@
 var btn = document.getElementById('btn');
 var mask = document.getElementById('mask');
 var popup = document.getElementById('popup');
+var data = {
+    "innerTop":0,
+    "innerBottom":0,
+    "innerLeft":0,
+    "innerRight":0,
+    "mouseX":0,
+    "mouseY":0,
+    "left":0,
+    "right":0
+};
 
 btn.onclick = function () {
     mask.style.display = 'block';
@@ -11,10 +21,8 @@ mask.onclick = function () {
     popup.style.display = 'none';
 };
 
-
 window.onscroll = abatvent;
 window.onresize = abatvent;
-popup.onmousedown = drag;
 //位置固定
 abatvent();     //初始化
 function abatvent() {
@@ -24,38 +32,71 @@ function abatvent() {
     popup.style.top = document.body.scrollTop + document.documentElement.clientHeight/2-100 + 'px';
 }
 
-
-
-//拖拽
-function drag(e) {
+//位置拖动
+popup.addEventListener('mousedown',dragMove);
+function dragMove(e) {
     var ev = e || event;
-    //边距
-    var disX = popup.offsetWidth -(ev.pageX - this.offsetLeft);
-    var disY = popup.offsetHeight - (ev.pageY - this.offsetTop);
-    ev.preventDefault();    //阻止默认
-    if (disX < 20 && disX > 0) {
-        document.addEventListener('mousemove',level)
+    //阻止默认
+    ev.preventDefault();
+    //点击时,弹窗距离屏幕左和上的距离
+    data.top = popup.offsetTop;
+    data.left = popup.offsetLeft;
+    //点击时,鼠标的位置
+    data.mouseX = ev.pageX;
+    data.mouseY = ev.pageY;
+    //点击时,鼠标在弹窗的内边距
+    data.innerTop = ev.pageY - popup.offsetTop;
+    data.innerLeft = ev.pageX - popup.offsetLeft;
+    data.innerRight = popup.offsetWidth -(ev.pageX - popup.offsetLeft);
+    data.innerBottom = popup.offsetHeight - (ev.pageY - popup.offsetTop);
+
+    if (data.innerRight < 20 && data.innerRight > 0 && data.innerTop > 20) {
+        document.addEventListener('mousemove',level);
     }
-    if (disY < 20 && disY > 0) {
+    if (data.innerBottom < 20 && data.innerBottom > 0) {
         document.addEventListener('mousemove',vertical);
     }
-    //水平缩放
-    function level (e) {
-        var ev = e || event;
-        popup.style.width = ev.clientX - popup.offsetLeft + disX + 'px';
-        popup.style.cursor = 'e-resize';
+    if(data.innerTop<=20){
+        document.addEventListener('mousemove',posMove);
     }
-    //垂直缩放
-    function vertical(e) {
-        var ev = e || event;
-        popup.style.height = ev.clientY - popup.offsetTop + disY + 'px';
-        popup.style.cursor = 's-resize';
-    }
+    //解绑
     document.onmouseup = unbundle;
-    //解绑拖拽
-    function unbundle() {
-        document.removeEventListener('mousemove',level);
-        document.removeEventListener('mousemove',vertical);
-        popup.style.cursor = 'default';
+}
+
+//移动函数
+function posMove(e) {
+    var ev = e || event;
+    popup.style.cursor = 'move';
+    popup.style.left = data.left + ev.pageX - data.mouseX + 'px';
+    popup.style.top = data.top + ev.pageY - data.mouseY + 'px';
+}
+//水平缩放
+function level (e) {
+    var ev = e || event;
+    var width = ev.pageX - popup.offsetLeft + data.innerRight;
+    if(width <= 300){
+        popup.style.width = '300px';    //最小宽度
+    }else{
+        popup.style.width = width + 'px';
     }
+    popup.style.cursor = 'e-resize';
+}
+//垂直缩放
+function vertical(e) {
+    var ev = e || event;
+    var height = ev.pageY - popup.offsetTop + data.innerBottom;
+    if(height <= 200){
+        popup.style.height = '200px';    //最小高度
+    }else {
+        popup.style.height = height + 'px';
+    }
+    popup.style.cursor = 's-resize';
+}
+//解绑函数
+function unbundle() {
+    document.removeEventListener('mousemove',level);
+    document.removeEventListener('mousemove',vertical);
+    document.removeEventListener('mousemove',posMove);
+    document.onmouseup = null;
+    popup.style.cursor = 'default';
 }
