368cee69a1ebe86d9a35507ee501f27d3668c67a
jiang <735231195@qq.com>
2017-03-25 13:28:49 +0800

    transition方法写动画

--- task34_new/task34.js
+++ task34_new/task34.js
@@ -6,16 +6,20 @@ function create() {
     }
     oTable.innerHTML += str;
 }
-create();
+create();   //创建格子
 
 var ball = document.getElementById('ball');
 var data = {
-    deg: 0
+    deg: 0,     //记录角度
+    flag: true  //开关防止动画连续多次触发
 };
 
 function control() {
     var command = this.value;
     switch (command){
+        case 'GO':
+            go(data.deg);
+            break;
         case 'TUN LEF':
             data.deg -= 90;
             turn();
@@ -27,6 +31,19 @@ function control() {
         case 'TUN BAC':
             data.deg += 180;
             turn();
+            break;
+
+        case 'TRA LEF':
+            go(180);
+            break;
+        case 'TRA TOP':
+            go(270);
+            break;
+        case 'TRA RIG':
+            go(0);
+            break;
+        case 'TRA BOT':
+            go(90);
             break;
 
         case 'MOV LEF':
@@ -75,30 +92,31 @@ function control() {
             break;
     }
 }
+
+//转向
 function turn() {
-    ball.classList.add('actived');
     ball.style.transform = 'rotate(' + data.deg + 'deg)';
 }
-
+//方向判定
 function dirJudge(deg) {
     var dir = deg % 360;
     switch (true){
         case dir === 0:
             return 0;
-            break;
         case dir === 90 || dir === -270:
             return 1;
-            break;
         case Math.abs(dir) === 180:
             return 2;
-            break;
         case dir === -90 || dir === 270:
             return 3;
-            break;
     }
 }
-
+//前进
 function go(deg) {
+    if(!data.flag){
+        return;
+    }
+    data.flag = false;      //开关防止动画连续多次触发
     var disX = ball.offsetLeft;
     var disY = ball.offsetTop;
     if(dirJudge(deg) === 0){
@@ -110,4 +128,21 @@ function go(deg) {
     }else if(dirJudge(deg) === 3){
         ball.style.top = disY - 30 +'px';
     }
+    ball.addEventListener('transitionend',function () {
+        data.flag = true;
+    })
 }
+//边界判定
+function confineJudge(deg) {
+    var disX = ball.offsetLeft;
+    var disY = ball.offsetTop;
+    switch (true){
+        case dirJudge(deg) === 0 || disX>=270:
+        case dirJudge(deg) === 1 || disY>=270:
+        case dirJudge(deg) === 2 || disX<=30:
+        case dirJudge(deg) === 3 || disY<=30:
+            alert('到头了');
+            return;
+    }
+}
+
